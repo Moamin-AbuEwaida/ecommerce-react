bb9dca39bdf34619c3565264eca130b7bcadb5d5
Moamin <moamin.salamah@gmail.com>
2023-04-24 17:37:44 +0200

    footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,13 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      <div>Footer</div>
-    </div>
+    <footer className="bg-black py-12">
+      <div className="container mx-auto">
+        <p className="text-white text-center">
+          Copyright &copy; Ecommerce shop 2023. All right reserved.
+        </p>
+      </div>
+    </footer>
   );
 };
 
