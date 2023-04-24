6ad3b3b5f8c2266036571e4a22f361208f7901a1
Moamin <moamin.salamah@gmail.com>
2023-04-24 17:33:09 +0200

    hero component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,10 +4,11 @@ import { FiTrash2 } from "react-icons/fi";
 import CartItem from "./CartItem";
 import { SidebarContext } from "../contexts/SidebarContext";
 import { CartContext } from "../contexts/CartContext";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   const { isOpen, handleClose } = useContext(SidebarContext);
-  const { cart, clearCart, total } = useContext(CartContext);
+  const { cart, clearCart, total, itemsAmount } = useContext(CartContext);
   // console.log(useContext(CartContext));
   return (
     <div
@@ -16,7 +17,9 @@ const Sidebar = () => {
       } w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}
     >
       <div className="flex items-center justify-between py-6 border-b">
-        <div className="uppercase text-sm font-semibold">Shopping Bag (0)</div>
+        <div className="uppercase text-sm font-semibold">
+          Shopping Bag ({itemsAmount})
+        </div>
         <div
           className="cursor-pointer w-8 h-8 flex justify-center items-center"
           onClick={handleClose}
@@ -25,7 +28,7 @@ const Sidebar = () => {
         </div>
       </div>
 
-      <div className="flex flex-col gap-y-2 h-[520px] lg:h-[600px] overflow-y-auto overflow-x-hidden border-b">
+      <div className="flex flex-col gap-y-2 h-[450px] overflow-y-auto overflow-x-hidden border-b">
         {cart.map((item) => {
           return <CartItem key={item.id} item={item} />;
         })}
@@ -45,6 +48,18 @@ const Sidebar = () => {
             <FiTrash2 />
           </div>
         </div>
+        <Link
+          to={"/"}
+          className="bg-gray-200 flex p-4 justify-center items-center text-primary w-full font-medium"
+        >
+          View cart
+        </Link>
+        <Link
+          to={"/"}
+          className="bg-black flex p-4 justify-center items-center text-white w-full font-medium"
+        >
+          Checkout
+        </Link>
       </div>
     </div>
   );
